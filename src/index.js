e16b619c4979780cefd001634db386e5d15837ec
Zalman <moishkugel@gmail.com>
2022-09-12 11:26:01 -0400

    commit tree 471fa051e3c571d3b29846c74af2f899c0838292

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +13,84 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+
+  const cardsDiv = document.querySelector('#toy-collection')
+
+  function createCard(toy){
+    const card = document.createElement('div')
+    card.className = 'card'
+    card.innerHTML = 
+      `<h2>${toy.name}</h2>
+      <img src='${toy.image}' class='toy-avatar'/>
+      <p>
+        <span class = likes>${toy.likes}</span> Likes
+      </p>
+      <button class='like-btn' id='${toy.id}'>Like ❤️</button>`
+    cardsDiv.appendChild(card)
+    card.lastChild.addEventListener('click', () => {
+      toy.likes += 1
+      card.querySelector('span').textContent = toy.likes
+      patchLike(toy)
+    })
+  }
+  
+    const newToyBtn = document.querySelector('.add-toy-form')
+    
+    newToyBtn.addEventListener('submit', handleSubmit)
+      
+    function handleSubmit(e) {
+      e.preventDefault();
+      let toyObj = {
+        name: e.target.name.value,
+        image: e.target.image.value,
+        likes: 0,
+      }
+      createCard(toyObj)
+      postNewToy(toyObj)
+
+    }
+
+    
+    function getAllToys () {
+      fetch('http://localhost:3000/toys')
+      .then(response => response.json())
+      .then((data) => {
+        const toyObjects = data
+        toyObjects.forEach((toy) =>{
+        createCard(toy)
+        })
+      })
+    }
+
+    function postNewToy(toyObj) {
+      fetch('http://localhost:3000/toys', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(toyObj)
+      })
+    }
+
+    function patchLike(toy){
+      fetch(`http://localhost:3000/toys/${toy.id}`, {
+        method: 'PATCH',
+        headers:
+        {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+    
+        body: JSON.stringify(toy)
+      })
+    }
+
+    getAllToys()
+
+
+  });
+
+
+
+
+
+
+
